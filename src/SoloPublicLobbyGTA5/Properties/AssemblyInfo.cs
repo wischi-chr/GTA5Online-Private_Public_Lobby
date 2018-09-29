5dceb63fe6eb55bca3f0567a5f988acd9f1bf573
Christian Wischenbart <c.wischenbart@bitoxide.com>
2018-09-29 20:38:55 +0200

    Version bumb to v1.1

--- src/SoloPublicLobbyGTA5/Properties/AssemblyInfo.cs
+++ src/SoloPublicLobbyGTA5/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SoloPublicLobbyGTA5")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Helps you to create a solo public lobby in GTA 5 Online")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SoloPublicLobbyGTA5")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
